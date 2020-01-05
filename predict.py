655d61c9df2ed2b6f8d42b68016b6af635bc4987
Jason <neulf@hotmail.com>
2020-01-05 17:22:21 +0800

    updated var's name.

--- predict.py
+++ predict.py
@@ -6,8 +6,8 @@ from sklearn.metrics import confusion_matrix
 from bayes_predict import NB_Accuracy, classify
 from lr_predict import LR_Accuracy
 
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-Y = np.array([1, 2, 1, 3, 1, 4])
+features_train = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+lablels_train = np.array([1, 2, 1, 3, 1, 4])
 
 #features_test = np.array([[-0.8, -1],[-0.7,-2]])
 #labels_test = np.array([1,1])
@@ -15,13 +15,13 @@ Y = np.array([1, 2, 1, 3, 1, 4])
 features_test = np.array([[-0.8, -1],[-0.7,-2]])
 labels_test = np.array([1,2])
 
-score = NB_Accuracy(X, Y, features_test, labels_test)
+score = NB_Accuracy(features_train, lablels_train, features_test, labels_test)
 print('GaussianNB %s' % score)
 
-X_train = X
-y_train = Y
+X_train = features_train
+y_train = lablels_train
 X_test = features_test
 y_test = labels_test
-cls = classify(X,Y)
+cls = classify(features_train, lablels_train)
 print('Traing score : %.2f' % cls.score(X_train, y_train))
 print('Testing score : %.2f' % cls.score(X_test, y_test))
