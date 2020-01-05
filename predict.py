619fad06d167e44d879692cebb29fd3c9b50e61d
Jason <neulf@hotmail.com>
2020-01-05 17:37:58 +0800

    updated var's name.

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ from bayes_predict import NB_Accuracy, classify
 from lr_predict import LR_Accuracy
 
 features_train = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-lablels_train = np.array([1, 2, 1, 3, 1, 4])
+labels_train = np.array([1, 2, 1, 3, 1, 4])
 
 #features_test = np.array([[-0.8, -1],[-0.7,-2]])
 #labels_test = np.array([1,1])
@@ -15,13 +15,13 @@ lablels_train = np.array([1, 2, 1, 3, 1, 4])
 features_test = np.array([[-0.8, -1],[-0.7,-2]])
 labels_test = np.array([1,2])
 
-score = NB_Accuracy(features_train, lablels_train, features_test, labels_test)
+score = NB_Accuracy(features_train, labels_train, features_test, labels_test)
 print('GaussianNB %s' % score)
 
 X_train = features_train
-y_train = lablels_train
+y_train = labels_train
 X_test = features_test
 y_test = labels_test
-cls = classify(features_train, lablels_train)
+cls = classify(features_train, labels_train)
 print('Traing score : %.2f' % cls.score(X_train, y_train))
 print('Testing score : %.2f' % cls.score(X_test, y_test))
